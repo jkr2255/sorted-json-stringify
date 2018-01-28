050ad5c43ddabb67800eff5bb3f64cb99bf30145
jkr2255 <jkr2255@users.noreply.github.com>
2018-01-28 21:11:30 +0900

    tune build settings

--- README.md
+++ README.md
@@ -14,4 +14,5 @@ It can be used by `import sortedJSONStringify from 'sorted-json-stringify'` (in 
 
 ## Changelog
 
+* 0.0.2 (2018/01/28) - change build settings (using uglifier, add `module` field in `package.json`)
 * 0.0.1 (2018/01/28) - first release
--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,15 @@
 // import nodeResolve from 'rollup-plugin-node-resolve';
 // import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
-  entry: 'src/index.js',
+  input: 'src/index.js',
   plugins: [
     // nodeResolve({ jsnext: true }), // npmモジュールを`node_modules`から読み込む
     // commonjs(), // CommonJSモジュールをES6に変換
-    babel() // ES5に変換
+    babel(), // ES5に変換
+    uglify()
   ],
   output: {
     file: 'index.js',
